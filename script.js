6e3313a2dbbff8571c34463231f26457fa545d40
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-10-24 21:34:37 +0200

    input color that changes body background

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const price = document.querySelector("#price");
 const output = document.querySelector(".price-output");
+const color = document.querySelector("#color")
+const body  = document.querySelector("body")
+
 
 output.textContent = price.value;
 
 price.addEventListener("input", () => {
   output.textContent = price.value;
 });
+
+
+color.addEventListener("input", () => {
+    body.style.backgroundColor = color.value;
+})
