3c976619fa0232b1e2b16c31f901f2beaf802f99
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-11-04 22:41:15 +0100

    basic structure for password comparison script

---
+++ script.js
@@ -0,0 +1,10 @@
+const p1 = document.querySelector('#password1')
+const p2 = document.querySelector('#password2')
+
+
+function comparePasswords(){
+    if(p1.textContent === p2.textContent){
+        alert("edgsdfh")
+    }
+}
+comparePasswords()
