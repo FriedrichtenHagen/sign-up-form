985b8a1e0d4e1adf58be2799d8c4781d800bfc56
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-11-05 17:00:25 +0100

    add required label after pw label

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const p1 = document.querySelector('#password1')
 const p2 = document.querySelector('#password2')
+const form1 = document.querySelector("form")
 
-
-function comparePasswords(){
-    if(p1.textContent === p2.textContent){
-        alert("edgsdfh")
+form1.addEventListener("submit", e => {
+    if(p1.textContent !== p2.textContent){
+       
     }
-}
-comparePasswords()
+})
+
