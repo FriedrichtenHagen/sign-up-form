6b12b03719308ad976fe9137f11dad45fa254e65
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-11-05 17:36:18 +0100

    create confirm password checker

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const p1 = document.querySelector('#password1')
-const p2 = document.querySelector('#password2')
 const form1 = document.querySelector("form")
+const pwlabel1 = document.querySelector("#pwlabel1")
+const pwlabel2 = document.querySelector("#pwlabel2")
 
 form1.addEventListener("submit", e => {
-    if(p1.textContent !== p2.textContent){
-       
+    const p1 = document.querySelector('#password1').value
+    const p2 = document.querySelector('#password2').value
+    if(p1 !== p2){
+        e.preventDefault()
+        pwlabel1.classList.add("required") 
+        pwlabel2.classList.add("required") 
     }
+
 })
+
 
