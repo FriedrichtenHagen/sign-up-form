a001e08ac88f7c397c6a26c80c45c13685be7073
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-29 16:57:09 +0100

    add js validation

--- script.js
+++ script.js
@@ -3,25 +3,47 @@ const pwlabel1 = document.querySelector("#pwlabel1")
 const pwlabel2 = document.querySelector("#pwlabel2")
 
 form1.addEventListener("submit", e => {
-    const p1 = document.querySelector('#password1').value
+    checkPasswordIdenticallity()
+    if (!email.validity.valid) {
+      showError();
+      e.preventDefault();
+    }
+})
+
+function checkPasswordIdenticallity(){
+  const p1 = document.querySelector('#password1').value
     const p2 = document.querySelector('#password2').value
     if(p1 !== p2){
         e.preventDefault()
         pwlabel1.classList.add("required") 
         pwlabel2.classList.add("required") 
     }
-
-})
+}
 
 // constraint validation API
 const email = document.getElementById("email");
+const emailError = document.querySelector(".error")
 
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
-  } else {
-    email.setCustomValidity("");
+  if(email.validity.valid){
+    emailError.textContent = ""
+    emailError.className = "error"
+  }else{
+    showError()
   }
 });
 
+
+function showError(){
+  if (email.validity.typeMismatch) {
+    emailError.textContent = "Please enter an email"
+  } 
+  else if(email.validity.valueMissing){
+    emailError.textContent = "You need to enter an email address!"
+  }
+  else if(email.validity.tooShort){
+    emailError.textContent = `Email should be at least ${email.minLength} characters. You entered ${email.value.length} characters`
+  }
+  emailError.className = "error active"
+}
 
